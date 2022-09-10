03c27e7a46de18cb3897dc78feb1bc53b6e3b9bf
deepa <prasanth@uplio.com>
2022-09-10 11:38:55 +0200

    first commit

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int numbers;
+	int i=1;
+	cout << "enter a number";
+	cin >> numbers;
+	while(i<=numbers)
+	{
+		cout << i;
+		i++;
+	}
+	return 0;
+}
+
